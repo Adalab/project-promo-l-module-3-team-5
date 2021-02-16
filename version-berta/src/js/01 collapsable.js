fb55fb5d60bbc8493d5398bea3774db443a8a8ec
ctalloc <cecilia.tallo@gmail.com>
2021-02-16 17:28:12 +0100

    Clean version beta code.

--- version-berta/src/js/01 collapsable.js
+++ version-berta/src/js/01 collapsable.js
@@ -6,6 +6,7 @@ function handleCollapsable(event) {
   const collapsableElement = event.currentTarget.closest(".collapsable");
   collapsableElement.classList.toggle("collapsable--on");
 }
+
 for (const legendElement of legendElements) {
   legendElement.addEventListener("click", handleCollapsable);
 }
