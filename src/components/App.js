3714d791a3dacc18acb3a4aa048d6becf7e64c88
bertapsan <bertapsan@gmail.com>
2021-02-18 16:17:59 +0100

    Add Design component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import '../stylesheets/App.scss';
 import React from 'react';
 import Header from './Header';
+import Design from './Design';
 import Footer from './Footer';
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ class App extends React.Component {
           <Form />
           <CardPreview />
         </Main> */}
+        <Design />
         <Footer />
       </div>
     );
