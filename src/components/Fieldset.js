b76808e04b445a2d1e050e400c0fbce377df9fb5
ctalloc <cecilia.tallo@gmail.com>
2021-02-18 18:34:37 +0100

    Create Colapsable and Fieldset modules.

---
+++ src/components/Fieldset.js
@@ -0,0 +1,13 @@
+import '../stylesheets/layout/_formulario.scss';
+import '../stylesheets/layout/_formulario.scss';
+import Colapsable from './Colapsable'
+
+const Fieldset = () => {
+    return (
+      <fieldset class="design/share/fill collapsable">
+          <Colapsable></Colapsable> 
+      </fieldset>
+    );
+  };
+
+export default Fieldset;
