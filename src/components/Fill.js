959b2534b6b5ad8a2c82eaa21b98031113ea6ce5
2y2son4 <adrianadelteso@gmail.com>
2021-02-19 18:11:43 +0100

    Add props to Collapsable component in Fill component.

--- src/components/Fill.js
+++ src/components/Fill.js
@@ -1,43 +1,21 @@
-import React from "react";
-import Input from "./Input";
-import BtnImg from "./BtnImg";
-import Colapsable from "./Colapsable";
-import "../stylesheets/layout/_rellena.scss";
+import React from 'react';
+import Input from './Input';
+import BtnImg from './BtnImg';
+import Colapsable from './Colapsable';
+import '../stylesheets/layout/_rellena.scss';
 
 const Fill = () => {
   return (
     <fieldset className="rellena collapsable">
-      <Colapsable></Colapsable>
-      {/* <include
-        src="./colapsable.html"
-        faclassName="far fa-keyboard"
-        title="Rellena"
-        jsLegend="legend__rellena">
-      </include> */}
+      <Colapsable jsLegend="legend__rellena" faClass="far fa-keyboard" title="Rellena"></Colapsable>
       <div className="collapsable--hide">
-        <Input
-          forId="completeName"
-          text="Nombre completo"
-          placeText="Ej: Sally Jill"
-          name="name"
-        />
+        <Input forId="completeName" text="Nombre completo" placeText="Ej: Sally Jill" name="name" />
 
-        <Input
-          forId="position"
-          text="Puesto"
-          placeText="Ej: Front-end unicorn"
-          name="job"
-        />
+        <Input forId="position" text="Puesto" placeText="Ej: Front-end unicorn" name="job" />
 
         <BtnImg />
 
-        <Input
-          forId="emailAddress"
-          text="Email"
-          placeText="Ej: sally.hill@gmail.com"
-          type="email"
-          name="email"
-        />
+        <Input forId="emailAddress" text="Email" placeText="Ej: sally.hill@gmail.com" type="email" name="email" />
 
         <Input
           forId="telephone"
@@ -49,19 +27,9 @@ const Fill = () => {
           required={false}
         />
 
-        <Input
-          forId="linkedin"
-          text="Linkedin"
-          placeText="Ej: linkedin.com/in/sally-hill"
-          name="linkedin"
-        />
+        <Input forId="linkedin" text="Linkedin" placeText="Ej: linkedin.com/in/sally-hill" name="linkedin" />
 
-        <Input
-          forId="github"
-          text="Github"
-          placeText="Ej: @sally-hill"
-          name="github"
-        />
+        <Input forId="github" text="Github" placeText="Ej: @sally-hill" name="github" />
       </div>
     </fieldset>
   );
