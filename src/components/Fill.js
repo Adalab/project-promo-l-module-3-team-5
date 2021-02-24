8fc8056a1e609b91b9dcadffaaaa2752a45418f0
2y2son4 <adrianadelteso@gmail.com>
2021-02-24 17:06:55 +0100

    Install propTypes

--- src/components/Fill.js
+++ src/components/Fill.js
@@ -1,34 +1,34 @@
 import React from 'react';
 import Input from './Input';
 import BtnImg from './BtnImg';
-import Colapsable from './Colapsable';
+// import PropTypes from 'prop-types';
 import '../stylesheets/layout/_rellena.scss';
 
 const Fill = () => {
   return (
-      <div className="collapsable--hide">
-        <Input forId="completeName" text="Nombre completo" placeText="Ej: Sally Jill" name="name" />
+    <div className="collapsable--hide">
+      <Input forId="completeName" text="Nombre completo" placeText="Ej: Sally Jill" name="name" />
 
-        <Input forId="position" text="Puesto" placeText="Ej: Front-end unicorn" name="job" />
+      <Input forId="position" text="Puesto" placeText="Ej: Front-end unicorn" name="job" />
 
-        <BtnImg />
+      <BtnImg />
 
-        <Input forId="emailAddress" text="Email" placeText="Ej: sally.hill@gmail.com" type="email" name="email" />
+      <Input forId="emailAddress" text="Email" placeText="Ej: sally.hill@gmail.com" type="email" name="email" />
 
-        <Input
-          forId="telephone"
-          text="Teléfono"
-          placeText="Ej: 555-55-55-55"
-          type="tel"
-          name="phone"
-          asterisk=""
-          required={false}
-        />
+      <Input
+        forId="telephone"
+        text="Teléfono"
+        placeText="Ej: 555-55-55-55"
+        type="tel"
+        name="phone"
+        asterisk=""
+        required={false}
+      />
 
-        <Input forId="linkedin" text="Linkedin" placeText="Ej: linkedin.com/in/sally-hill" name="linkedin" />
+      <Input forId="linkedin" text="Linkedin" placeText="Ej: linkedin.com/in/sally-hill" name="linkedin" />
 
-        <Input forId="github" text="Github" placeText="Ej: @sally-hill" name="github" />
-      </div>
+      <Input forId="github" text="Github" placeText="Ej: @sally-hill" name="github" />
+    </div>
   );
 };
 
