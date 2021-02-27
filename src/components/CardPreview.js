781b8b0282b563ea508bc02572563c98a904f1c2
2y2son4 <adrianadelteso@gmail.com>
2021-02-27 13:07:16 +0100

    Add palette01 class to CardPreview <article>

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -5,7 +5,7 @@ import '../stylesheets/layout/_preview.scss';
 class CardPreview extends React.Component {
   render() {
     return (
-      <article className="preview__card">
+      <article className="preview__card palette01">
         <div className="card__name color--side">
           <h2 className="card__name--fullname color--fullname">Nombre Apellidos</h2>
           <p className="card__name--ocupation ">Front-end developer</p>
@@ -14,12 +14,12 @@ class CardPreview extends React.Component {
         <nav>
           <ul className="card__icons">
             <li>
-              <a href={this.props.data.phone || ''} className="js-phonePreview">
+              <a href={this.props.data.phone || ''}>
                 <i className="card__icons--icon color--icon fas fa-mobile-alt"></i>
               </a>
             </li>
             <li>
-              <a href={this.props.data.email || ''} className="js-emailPreview">
+              <a href={this.props.data.email || ''}>
                 <i className="card__icons--icon color--icon far fa-envelope"></i>
               </a>
             </li>
