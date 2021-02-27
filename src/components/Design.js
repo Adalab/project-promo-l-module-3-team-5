7959bdcd3d5c25fd09143fc33f8c7b7ab9564a80
2y2son4 <adrianadelteso@gmail.com>
2021-02-27 12:32:45 +0100

    Delete 'js' classes from Design component

--- src/components/Design.js
+++ src/components/Design.js
@@ -4,33 +4,26 @@ import '../stylesheets/layout/_formulario.scss';
 
 const Design = () => {
   return (
-      <div className="collapsable--hide">
-        <div className="design__radio">
-          <h3 className="design__radio--title">Colores</h3>
+    <div className="collapsable--hide">
+      <div className="design__radio">
+        <h3 className="design__radio--title">Colores</h3>
 
-          <input
-            name="palette"
-            className="design__radio--input js-palette"
-            type="radio"
-            id="paletteOne"
-            value="01"
-            defaultChecked
-          />
-          <label className="design__radio--label label__palette label__palette--One" htmlFor="paletteOne">
-            Palette One
-          </label>
+        <input name="palette" className="design__radio--input" type="radio" id="paletteOne" value="01" defaultChecked />
+        <label className="design__radio--label label__palette label__palette--One" htmlFor="paletteOne">
+          Palette One
+        </label>
 
-          <input name="palette" className="design__radio--input js-palette" type="radio" id="paletteTwo" value="02" />
-          <label className="design__radio--label label__palette label__palette--Two" htmlFor="paletteTwo">
-            Palette Two
-          </label>
+        <input name="palette" className="design__radio--input" type="radio" id="paletteTwo" value="02" />
+        <label className="design__radio--label label__palette label__palette--Two" htmlFor="paletteTwo">
+          Palette Two
+        </label>
 
-          <input name="palette" className="design__radio--input js-palette" type="radio" id="paletteThree" value="03" />
-          <label className="design__radio--label label__palette label__palette--Three" htmlFor="paletteThree">
-            Palette Three
-          </label>
-        </div>
+        <input name="palette" className="design__radio--input" type="radio" id="paletteThree" value="03" />
+        <label className="design__radio--label label__palette label__palette--Three" htmlFor="paletteThree">
+          Palette Three
+        </label>
       </div>
+    </div>
   );
 };
 
