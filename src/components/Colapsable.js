1184bf7feb39b7b37ede6c0f8349e63057613aeb
2y2son4 <adrianadelteso@gmail.com>
2021-03-02 17:24:56 +0100

    Update docs/

--- src/components/Colapsable.js
+++ src/components/Colapsable.js
@@ -4,21 +4,24 @@ import '../stylesheets/layout/_formulario.scss';
 class Colapsable extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick=this.handleClick.bind(this);
-    
+    this.handleClick = this.handleClick.bind(this);
   }
-  handleClick(){
-    this.props.functionColapsable()
-  };
-    render() {
-      return (
-        <legend className={"legend "+this.props.jsLegend} title="click aquí para abrir/cerrar" onClick={this.handleClick} >
-            <i className={"legend__icon--main "+this.props.faClass}></i>
-             <h3 className="legend__title">{this.props.title}</h3> 
-            <i className="legend__icon--arrow collapsable--close fas fa-angle-down"></i>
-        </legend>
-      );
-    }
+  handleClick() {
+    this.props.functionColapsable();
   }
+  render() {
+    return (
+      <legend
+        className={'legend ' + this.props.jsLegend}
+        title="click aquí para abrir/cerrar"
+        onClick={this.handleClick}
+      >
+        <i className={'legend__icon--main ' + this.props.faClass}></i>
+        <h3 className="legend__title">{this.props.title}</h3>
+        <i className="legend__icon--arrow collapsable--close fas fa-angle-down"></i>
+      </legend>
+    );
+  }
+}
 
 export default Colapsable;
