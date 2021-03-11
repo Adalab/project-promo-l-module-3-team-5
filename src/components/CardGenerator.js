d26c0f99f67797bac5ebf3ee057c5dd8bdf7c81a
ctalloc <cecilia.tallo@gmail.com>
2021-03-11 12:40:23 +0100

    Set and get from LocalStorage.

--- src/components/CardGenerator.js
+++ src/components/CardGenerator.js
@@ -5,6 +5,8 @@ import BtnReset from './BtnReset';
 import CardPreview from './CardPreview';
 import Form from './Form';
 import Footer from './Footer';
+import ls from '../services/localStorage';
+
 
 class CardGenerator extends React.Component {
   constructor(props) {
@@ -22,6 +24,52 @@ class CardGenerator extends React.Component {
     this.handlePalette = this.handlePalette.bind(this);
     this.handleReset = this.handleReset.bind(this);
   }
+
+  componentDidUpdate() {
+    // escribir en el local storage lo hacemo cuando el componente ya ha sido actualizado
+    ls.set('name', this.state.name);
+    ls.set('job', this.state.job);
+    ls.set('email', this.state.email);
+    ls.set('phone', this.state.phone);
+    ls.set('linkedin', this.state.linkedin);
+    ls.set('github', this.state.github);
+    ls.set('palette', this.state.palette);
+  }
+
+  componentDidMount() {
+    // leer del local storage lo hacemos en el componentDidMount
+    const localStorageName = ls.get('name');
+    if (localStorageName) {
+      this.setState({name: localStorageName});
+    }
+    const localStorageJob = ls.get('job');
+    if (localStorageJob) {
+      this.setState({job: localStorageJob});
+    }
+    const localStorageEmail = ls.get('email');
+    if (localStorageEmail) {
+      this.setState({email: localStorageEmail});
+    }
+    const localStoragePhone = ls.get('phone');
+    if (localStoragePhone) {
+      this.setState({phone: localStoragePhone});
+    }
+    const localStorageLinkedin = ls.get('linkedin');
+    if (localStorageLinkedin) {
+      this.setState({linkedin: localStorageLinkedin});
+    }
+    const localStorageGithub = ls.get('github');
+    if (localStorageGithub) {
+      this.setState({github: localStorageGithub});
+    }
+    const localStoragePalette = ls.get('palette');
+    if (localStoragePalette) {
+      this.setState({palette: localStoragePalette});
+    }
+  }
+
+
+
   handleReset() {
     console.log('Me han clickado');
     this.setState({ name: '', job: '', email: '', phone: '', linkedin: '', github: '', palette: '01' });
