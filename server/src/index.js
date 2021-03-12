bcac537a74db53bd2985629284a36144899a5445
ctalloc <cecilia.tallo@gmail.com>
2021-03-12 17:59:07 +0100

    Tinkering with in.js in the server.

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-// const api = require('../.././web/src/services/api.js');
+const express = require("express");
+const cors = require("cors");
+const api = require("../.././web/src/services/api.js");
 
 const server = express();
 
@@ -16,9 +16,8 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-const staticServerPath = '../.././web/public'; // relative to the root of the project
+const staticServerPath = "../.././web/public"; // relative to the root of the project
 server.use(express.static(staticServerPath));
-
 
 // API request > GET > http://localhost:3000/users
 // server.get('/users', (req, res) => {
@@ -29,11 +28,29 @@ server.use(express.static(staticServerPath));
 // });
 
 // API request > POST > http://localhost:3000/card
-server.post('/card', (req, res) => {
-  // console request body params
+server.post("/card", (req, res) => {
   console.log(`Creating the user in database with user name: "${req.body}"`);
-  const response = {
-    result: `Card created: ${req.body.cardURL}`,
+  api.sendData(userData).then(data=>{return data})
+  const card = () => {
+    if (data !== undefined) {
+      console.log(card);
+      res.json({
+        error: false,
+        data: data.name,
+      });
+    }
+    else {
+      // Si el usuario no existe devuelvo un error
+      res.status(404).json({
+        error: 'undefined data',
+        message: 'undefined data'
+      });
+    }
   };
-  res.json(response);
+
+  // console.log(`Creating the user in database with user name: "${req.body}"`);
+  // const response = {
+  //   result: `Card created: ${req.body.cardURL}`,
+  // };
+  // res.json(response);
 });
